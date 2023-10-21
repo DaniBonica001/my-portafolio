df0793af361461bfec3b3a89a54fde87bc1c5f8e
DaniBonica001 <danielabc_0106@hotmail.com>
2023-10-21 00:50:51 -0500

    fixed: import services

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import NavBar from "./Components/NavBar";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Skills from "./Components/Skills";
-import Services from "./Components/Services";
 import Portafolio from "./Components/Portafolio";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
